e7542438e4f73e2d99993d9ddc1a08227bc18265
ChaSangHyun <chacar0703@naver.com>
2016-05-15 01:58:30 +0900

    바람에 따른 애니메이션 적용

---
+++ Assets/02.Script/CAnim/CWindAnim.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CWindAnim : MonoBehaviour {
+
+    public CWindMgr _windMgr;
+    private Animator _anim;
+
+    void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        _windMgr = GameObject.Find("WindMgr").GetComponent<CWindMgr>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    
+        if(_windMgr._windSpeed == CWindMgr.WINDSPEED.SLOW)
+        {
+            _anim.SetBool("Slow", true);
+            _anim.SetBool("Mid", false);
+            _anim.SetBool("Fast", false);
+            
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 45 * (int)_windMgr._windDir, 0), 0.3f * Time.deltaTime);
+            
+        }
+        else if (_windMgr._windSpeed == CWindMgr.WINDSPEED.NORMAL)
+        {
+            _anim.SetBool("Slow", false);
+            _anim.SetBool("Mid", true);
+            _anim.SetBool("Fast", false);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 45 * (int)_windMgr._windDir, 0), 0.3f * Time.deltaTime);
+        }
+        else if (_windMgr._windSpeed == CWindMgr.WINDSPEED.FAST)
+        {
+            _anim.SetBool("Slow", false);
+            _anim.SetBool("Mid", false);
+            _anim.SetBool("Fast", true);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 45 * (int)_windMgr._windDir, 0), 0.3f * Time.deltaTime);
+        }
+    }
+
+    public void Hit()
+    {
+        _anim.SetTrigger("Hit");
+    }
+}
